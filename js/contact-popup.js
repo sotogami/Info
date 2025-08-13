70597e739789199b31bc96f1cfa5d87e6e4ae602
sotogami <ruitonau@gmail.com>
2025-08-13 16:32:11 +0900

    contact 팝업 css 수정 1

--- js/contact-popup.js
+++ js/contact-popup.js
@@ -1,23 +1,24 @@
 $(document).ready(function () {
   const $contactLink = $('#contact-link');
   const $contactPopupContainer = $('#contact-popup');
-  
+
   $contactLink.on('click', function (e) {
     e.preventDefault();
 
     $.get('html/contact-popup.html', function (data) {
-      $contactPopupContainer.html(data).fadeIn(); // 팝업을 삽입하고 나타나게 함
+      $contactPopupContainer.html(data).fadeIn(); // 팝업 삽입 후 보여주기
     });
   });
 
-
+  // 닫기 버튼 클릭 시
   $contactPopupContainer.on('click', '#close-popup', function () {
-    $contactPopupContainer.fadeOut(); // 팝업 닫기
+    $contactPopupContainer.fadeOut();
   });
 
-  $(window).on('click', function (e) {
+  // 팝업 외부 클릭 시 닫기
+  $contactPopupContainer.on('click', function (e) {
     if ($(e.target).is($contactPopupContainer)) {
-      $contactPopupContainer.fadeOut(); // 팝업 닫기
+      $contactPopupContainer.fadeOut();
     }
   });
 });
